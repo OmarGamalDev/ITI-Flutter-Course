40aa823016af77d031fda030c7fb775fc07df048
Omar Gamal <omarthefirst6206@gmail.com>
2025-07-25 02:06:36 +0300

    start

---
+++ Day 10/education_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EducationApp());
+}
+
+class EducationApp extends StatelessWidget {
+  const EducationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
